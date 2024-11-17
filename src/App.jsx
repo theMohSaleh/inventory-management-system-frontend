61555361ca0a7b4e27527b8ac0d944a905e29d1c
Mohamed Saleh <mohxbox36021@gmail.com>
2024-11-17 10:16:00 +0300

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+// App.jsx
+
+import { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import NavBar from './components/NavBar/NavBar';
+import Landing from './components/Landing/Landing';
+import Dashboard from './components/Dashboard/Dashboard';
+import SignupForm from './components/SignupForm/SignupForm'
+import SigninForm from './components/SigninForm/SigninForm'
+import * as authService from '../src/services/authService';
+
+const App = () => {
+  const [user, setUser] = useState(authService.getUser());
+
+  return (
+    <>
+      <NavBar user={user} />
+      <Routes>
+        { user ? (
+          <Route path="/" element={<Dashboard user={user} />} />
+        ) : (
+          <Route path="/" element={<Landing />} />
+        )}
+        <Route path='/signup' element={<SignupForm setUser={setUser}/>} />
+        <Route path='/signin' element={<SigninForm setUser={setUser} />} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
